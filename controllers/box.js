54bfe1ae92991f83ae0c52904fc42a8c689be0a4
vineeth <vineethvijaysynnefo@gmail.com>
2024-10-03 15:59:16 +0530

    generateboxserialno api created

--- controllers/box.js
+++ controllers/box.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose"); // Make sure to import mongoose
+const mongoose = require("mongoose");
 const boxSerialNo = require("../Models/BoxSerialNo");
 const utils = require("../controllers/utils");
 const shortid = require('shortid');
@@ -7,37 +7,33 @@ exports.generateBoxSerialNo = async (req, res) => {
     try {
         const { hubID, qnty } = req.body;
 
-        // Validate input
         if (!hubID || !qnty || typeof qnty !== 'number' || qnty <= 0) {
             return utils.commonResponse(res, 400, "Invalid input parameters");
         }
 
-        // Generate an array of unique short IDs for the box serial numbers
-        const serialNos = new Array(qnty).fill(0).map(() => shortid.generate());
+        const existingBoxSerial = await boxSerialNo.findOne({ hubID });
+        let serialCounter = existingBoxSerial ? existingBoxSerial.serialNos.length + 1 : 1;
 
-        // Generate a new boxID (ObjectId)
-        const boxID = new mongoose.Types.ObjectId();
-
-        // Find the last created box entry to get the last box number
-        const lastBoxEntry = await boxSerialNo.findOne().sort({ createdAt: -1 }).exec();
-        let initialBoxNumber = 1001; // Default starting number
-
-        // If a last box entry is found, calculate the next initial box number
-        if (lastBoxEntry) {
-            initialBoxNumber = lastBoxEntry.initialBoxNumber + lastBoxEntry.serialNumber; // Start from the last entry
-        }
-
-        // Create a new entry in the database
-        await boxSerialNo.create({
-            boxID: boxID,
-            serialNumber: qnty,
-            serialNos: serialNos,
-            initialBoxNumber: initialBoxNumber // Store only the initial box number
+        const boxSerialNos = Array.from({ length: qnty }, () => {
+            const serialNo = shortid.generate();
+            const sixDigitNo = serialCounter.toString().padStart(6, '0');
+            serialCounter += 1;
+            return `${serialNo}${sixDigitNo}`;
         });
 
-        // Return success response with the starting box number
-        utils.commonResponse(res, 200, "Box serial number generated", { hubID, boxID, serialNos, boxNumber: initialBoxNumber });
+        if (existingBoxSerial) {
+            await boxSerialNo.updateOne(
+                { hubID },
+                { 
+                    $push: { serialNos: { $each: boxSerialNos } }, 
+                    $inc: { serialNoCount: qnty } 
+                }
+            );
+        } else {
+            await boxSerialNo.create({ hubID, serialNos: boxSerialNos, serialNoCount: qnty });
+        }
 
+        utils.commonResponse(res, 200, "Box serial numbers generated", { hubID, boxSerialNos });
     } catch (error) {
         utils.commonResponse(res, 500, "Unexpected server error", error.toString());
     }
