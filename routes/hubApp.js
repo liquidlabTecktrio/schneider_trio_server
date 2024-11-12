9e8c7fd3ad6099325e5c2281b266b27c34545638
Praveenchandra <praveenchandra514@gmail.com>
2024-11-12 17:04:56 +0530

    generatePartSerialNo routes

--- routes/hubApp.js
+++ routes/hubApp.js
@@ -5,24 +5,40 @@ const BoxSerialNoController = require("../controllers/box");
 const ProjectController = require("../controllers/projects");
 const ComponentController = require("../controllers/components");
 
-router.post("/generateComponentSerialNo",serialNoController.generateComponentSerialNo);
+router.post(
+  "/generateComponentSerialNo",
+  serialNoController.generateComponentSerialNo
+);
+router.post(
+  "/generateComponentSerialNo",
+  serialNoController.generatePartSerialNo
+);
 router.post("/generatePanelSerialNo", serialNoController.generatePanelSerialNo);
-router.post("/generateBoxSerialNo", BoxSerialNoController.generateBoxSerialNo); 
+router.post("/generateBoxSerialNo", BoxSerialNoController.generateBoxSerialNo);
 router.post("/addBoxToProject", BoxSerialNoController.addBoxToProject);
 router.post("/addComponentsToBoxes", BoxSerialNoController.addComponentsToBox);
 
 //project
 router.post("/getAllProjects", ProjectController.getAllProjects);
-router.post("/getProjectDetailsWithParts",ProjectController.getProjectDetailsWithParts);
+router.post(
+  "/getProjectDetailsWithParts",
+  ProjectController.getProjectDetailsWithParts
+);
 
 // router.post("/getAllProjects", ProjectController.getAllProjects);
 router.post("/getProjectsDetails", ProjectController.getProjectsDetails);
 router.post("/componentScanResult", ProjectController.getComponentScanResult);
-router.post("/incrementFixedQuantity",ProjectController.getincrementFixedQuantity);
+router.post(
+  "/incrementFixedQuantity",
+  ProjectController.getincrementFixedQuantity
+);
 
 //spoke
 router.post("/getAllSpokeProjects", ProjectController.getAllSpokeProjects);
-router.post("/getSpokeProjectsDetails",ProjectController.getSpokeProjectsDetails);
+router.post(
+  "/getSpokeProjectsDetails",
+  ProjectController.getSpokeProjectsDetails
+);
 
 // Boxes
 router.post("/getBoxDetails", BoxSerialNoController.getBoxDetails);
@@ -30,8 +46,10 @@ router.post("/shipProject", ProjectController.shipProject);
 router.post("/updateBoxStatus", BoxSerialNoController.updateBoxStatus);
 
 //
-router.post("/getBoxDetailsBasedOnComponentScan",ComponentController.getBoxDetailsBasedOnComponentScan);
-
+router.post(
+  "/getBoxDetailsBasedOnComponentScan",
+  ComponentController.getBoxDetailsBasedOnComponentScan
+);
 
 // new Routes For PartsAddToBox
 router.post("/addPartsToBoxes", BoxSerialNoController.addPartsToBox);
