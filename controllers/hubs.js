2421a08787b880c9bde438047ed9d4bace8d994e
tecktrio <amalpullan4@gmail.com>
2025-01-02 12:49:51 +0530

    updated doc

--- controllers/hubs.js
+++ controllers/hubs.js
@@ -42,15 +42,17 @@ exports.createHubUser = async (req, res) => {
             "All fields are required: username, password, phonenumber, hub_id"
             );
         }
-        let hub = await Hubs.findById(hub_id)
-        console.log(hub)
-        hub.HubUsers.push({username, password, phonenumber})
-        await hub.save().then(async (result) => {
-            const allHubs = await Hubs.find();
-            utils.commonResponse(res, 200, "hub user created successfully", allHubs);
-        }).catch((err) => {
-            utils.commonResponse(res, 401, "unexpected server error", err.toString());
-        });
+        const result = await Hubs.findByIdAndUpdate(
+            hubId,
+            {
+              $push: { HubUsers: newUser },
+            },
+            { new: true } // Returns the updated document
+          );
+       
+        const allHubs = await Hubs.find();
+        utils.commonResponse(res, 200, "hub user created successfully", allHubs);
+       
     }
     catch (error) {
         utils.commonResponse(res, 500, "unexpected server error", error.toString());
