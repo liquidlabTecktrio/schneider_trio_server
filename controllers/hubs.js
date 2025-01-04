2eeaf2ca9022dcf3fecd0da2f8baa14f39cd03e5
tecktrio <amalpullan4@gmail.com>
2025-01-04 12:14:39 +0530

    removed token expiry

--- controllers/hubs.js
+++ controllers/hubs.js
@@ -7,20 +7,20 @@ const HubUsers = require("../Models/HubUsers");
 
 const generateToken = async (hub_ID) => {
     const token = await jwt.sign({ hub_ID: hub_ID }, process.env.JWT_SECRET);
-  
+
     return token;
-  };
+};
 
 
-  exports.createHubs = async (req, res) => {
+exports.createHubs = async (req, res) => {
     // THIS FUNCTION WILL CREATE NEW HUB
     try {
         console.log(req.body)
-        const { hubName, hubShortName, hubUsername, hubPassword , logo_ZPL } = req.body;
-        let result  = await Hubs.create({ hubName, hubShortName, logo_ZPL})
-        await HubUsers.create({ username:hubUsername, password:hubPassword, level:1, hub_id:result._id})
+        const { hubName, hubShortName, hubUsername, hubPassword, logo_ZPL } = req.body;
+        let result = await Hubs.create({ hubName, hubShortName, logo_ZPL })
+        await HubUsers.create({ username: hubUsername, password: hubPassword, level: 1, hub_id: result._id })
         const allHubs = await Hubs.find();
-        return utils.commonResponse(res, 200, "hub created successfully",allHubs);
+        return utils.commonResponse(res, 200, "hub created successfully", allHubs);
     }
     catch (error) {
         utils.commonResponse(res, 500, "unexpected server error", error.toString());
@@ -30,28 +30,30 @@ const generateToken = async (hub_ID) => {
 exports.createHubUser = async (req, res) => {
     // THIS FUNCTION WILL CREATE NEW HUB
     try {
-        
-        const { username, password, phonenumber, hub_id} = req.body;
-            // Validate required fields
+
+        const { username, password, phonenumber, hub_id } = req.body;
+        // Validate required fields
         if (!username || !password || !phonenumber || !hub_id) {
             return utils.commonResponse(
-            res,
-            400,
-            "All fields are required: username, password, phonenumber, hub_id"
+                res,
+                400,
+                "All fields are required: username, password, phonenumber, hub_id"
             );
         }
-       let newUser  =  {
-        "username":username, 
-        "password":password,
-        "phonenumber":phonenumber,
-        "level":2,
-        "hub_id":hub_id
+        if (existinguser)utils.commonResponse(res, 201, "username already exist , try using another username");
+        let newUser = {
+            "username": username,
+            "password": password,
+            "phonenumber": phonenumber,
+            "level": 2,
+            "hub_id": hub_id
         }
-        const result = await HubUsers.create(newUser)
-       
+        let existinguser = await HubUsers.find({ username })
+        
+        await HubUsers.create(newUser)
         const allHubs = await Hubs.find();
         utils.commonResponse(res, 200, "hub user created successfully", allHubs);
-       
+
     }
     catch (error) {
         utils.commonResponse(res, 500, "unexpected server error", error.toString());
@@ -61,21 +63,21 @@ exports.createHubUser = async (req, res) => {
 exports.getAllHubUser = async (req, res) => {
     // THIS FUNCTION WILL CREATE NEW HUB
     try {
-        
-        const {hub_id} = req.body;
-            // Validate required fields
+
+        const { hub_id } = req.body;
+        // Validate required fields
         if (!hub_id) {
             return utils.commonResponse(
-            res,
-            400,
-            "All fields are required: hub_id"
+                res,
+                400,
+                "All fields are required: hub_id"
             );
         }
-        let hubUsers = await HubUsers.find({hub_id, level:2})
+        let hubUsers = await HubUsers.find({ hub_id, level: 2 })
         // console.log(hub)
         // hub.HubUsers.push({username, password, phonenumber})
         // await hub.save().then(async (result) => {
-            // const allHubs = await Hubs.find();
+        // const allHubs = await Hubs.find();
         utils.commonResponse(res, 200, "hub users fetched successfully", hubUsers);
         // }).catch((err) => {
         //     utils.commonResponse(res, 401, "unexpected server error", err.toString());
@@ -96,6 +98,7 @@ exports.deleteHub = async (req, res) => {
         hubID = new mongoose.Types.ObjectId(hubID)
         let result = await Hubs.deleteOne({ _id: hubID });
         if (result.deletedCount > 0) {
+            await HubUsers.deleteMany({hub_id:hubID})
             utils.commonResponse(res, 200, "Hub deleted successfully");
         } else {
             utils.commonResponse(res, 404, "Hub not found or already deleted");
@@ -110,12 +113,12 @@ exports.deleteHub = async (req, res) => {
 exports.updateHub = async (req, res) => {
     // THIS FUNCTION WILL DELETE EXISTNG HUB
     try {
-        let { hubID,logo_ZPL,isHubActive} = req.body;
+        let { hubID, logo_ZPL, isHubActive } = req.body;
         if (!hubID) {
             return utils.commonResponse(res, 400, "Missing hubID");
         }
         hubID = new mongoose.Types.ObjectId(hubID)
-        let hub = await Hubs.updateOne({ _id: hubID },{logo_ZPL, isHubActive});
+        let hub = await Hubs.updateOne({ _id: hubID }, { logo_ZPL, isHubActive });
         if (hub) {
             utils.commonResponse(res, 200, "Hub updated successfully");
         } else {
@@ -142,8 +145,8 @@ exports.getAllHubs = async (req, res) => {
 exports.activateHubUser = async (req, res) => {
     // THIS FUNCTION WILL RESPPOND WITH ALL THE AVAILABLE HUBS
     try {
-        const {user_id} = req.body
-        await HubUsers.updateOne({_id:user_id},{isActive:true});
+        const { user_id } = req.body
+        await HubUsers.updateOne({ _id: user_id }, { isActive: true });
         let hubusers = await HubUsers.find()
         utils.commonResponse(res, 200, "User Activated successfully", hubusers);
 
@@ -156,8 +159,8 @@ exports.activateHubUser = async (req, res) => {
 exports.deactivateHubUser = async (req, res) => {
     // THIS FUNCTION WILL RESPPOND WITH ALL THE AVAILABLE HUBS
     try {
-        const {user_id} = req.body
-        await HubUsers.updateOne({_id:user_id},{isActive:false});
+        const { user_id } = req.body
+        await HubUsers.updateOne({ _id: user_id }, { isActive: false });
         let hubusers = await HubUsers.find()
         utils.commonResponse(res, 200, "User Deactivated successfully", hubusers);
 
@@ -173,36 +176,36 @@ exports.LoginToHubs = async (req, res) => {
     try {
         const { hubUsername, hubPassword } = req.body;
         const user = await HubUsers.findOne({ username: hubUsername, password: hubPassword });
-        if(user){
-            if(!user.isActive){
+        if (user) {
+            if (!user.isActive) {
                 return utils.commonResponse(res, 201, "User Is not Active. Contact Hub Admin");
-             }
-             const hub = await Hubs.findById(user.hub_id)
-             if (hub) {
-                 const token = await generateToken(hub._id);
-     
-                 let resdata = {
-                     "hubName": hub.hubName,
-                     "hubShortName": hub.hubShortName,
-                     "hubUsername": user.username,
-                     "isHubActive": hub.isHubActive,
-                     "logo_ZPL": hub.logo_ZPL,
-                     "token":token,
-                     "level":user.level,
-                     "_id":hub._id,
-                 }
-     
-                 utils.commonResponse(res, 200, "Login successfully", resdata);
-             } else {
-                 utils.commonResponse(res, 401, "Something is Not Right");
-             }
+            }
+            const hub = await Hubs.findById(user.hub_id)
+            if (hub) {
+                const token = await generateToken(hub._id);
+
+                let resdata = {
+                    "hubName": hub.hubName,
+                    "hubShortName": hub.hubShortName,
+                    "hubUsername": user.username,
+                    "isHubActive": hub.isHubActive,
+                    "logo_ZPL": hub.logo_ZPL,
+                    "token": token,
+                    "level": user.level,
+                    "_id": hub._id,
+                }
+
+                utils.commonResponse(res, 200, "Login successfully", resdata);
+            } else {
+                utils.commonResponse(res, 401, "Something is Not Right");
+            }
         }
-        else{
-      
-                utils.commonResponse(res, 401, "Invalid username or password");
-            
+        else {
+
+            utils.commonResponse(res, 401, "Invalid username or password");
+
         }
-       
+
 
     } catch (error) {
         utils.commonResponse(res, 500, "Unexpected server error", error.toString());
