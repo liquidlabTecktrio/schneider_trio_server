256098276f8c52e460cdc47478511710b1eb5639
tecktrio <amalpullan4@gmail.com>
2025-01-06 16:21:42 +0530

    added new api in spoke for getting project  detials with parts

--- routes/spokesApp.js
+++ routes/spokesApp.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const verifyToken = require("../Middleware"); // Middleware for token verification
 const ProjectController = require("../controllers/projects");
 const SpokeController = require("../controllers/spoke");
-
+const BoxSerialNoController = require("../controllers/box");
 // Authentication
 router.post("/spokelogin", SpokeController.LoginToSpoke);
 
